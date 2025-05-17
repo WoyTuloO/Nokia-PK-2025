2aa9b7efef4506aa30790556637a3944c60f6331
AMDevRadeon <100355451+AMDevRadeon@users.noreply.github.com>
2025-05-17 22:27:12 +0200

    fix: change TODO macro into variadic one

--- src/COMMON/Utils/todo.h
+++ src/COMMON/Utils/todo.h
@@ -8,7 +8,7 @@
        F(__VA_ARGS__)
 #  define DO_PRAGMA(X) \
        _Pragma(#X)
-#  define TODO(MESSAGE) \
+#  define TODO(MESSAGE...) \
        DO_PRAGMA(message ("\n    \033[1;33mTODO\033[0m [" DEFER(STR, __FILE__) ":" DEFER(STR, __LINE__) "]: \033[1;37m" #MESSAGE "\033[0m"))
 # else
 #  define TODO(...)
