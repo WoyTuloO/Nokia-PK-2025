5a68ee028de9451ab729c2163693c354cac98ef4
AMDevRadeon <100355451+AMDevRadeon@users.noreply.github.com>
2025-05-19 01:54:17 +0200

    feat: countdown functionality now properly logs

--- src/UE/Application/Ports/TimerPort.cpp
+++ src/UE/Application/Ports/TimerPort.cpp
@@ -37,18 +37,27 @@ void TimerPort::startCountdown(Duration duration)
     {
         std::cv_status wait_result {};
         std::unique_lock<std::mutex> mutex_uniq_cv(this->countdown_guard);
+        const auto wait_start {std::chrono::steady_clock::now()};
         wait_result = this->countdown_cv.wait_for(mutex_uniq_cv, duration);
+        const auto wait_end {std::chrono::steady_clock::now()};
+
+        const std::chrono::duration<double> wait_duration {wait_end - wait_start};
 
         switch(wait_result)
         {
             case std::cv_status::timeout:;
             {
+                this->logger.logDebug("Countdown finished, handling timeout");
                 this->handler->handleTimeout();
                 return;
             }
             break;
             case std::cv_status::no_timeout:;
             {
+                this->logger.logDebug(
+                    std::format("Stopped timer after {} seconds (was waiting for {} seconds)", 
+                        wait_duration, 
+                        std::chrono::duration_cast<std::chrono::seconds>(duration)));
                 return;
             }
             break;
