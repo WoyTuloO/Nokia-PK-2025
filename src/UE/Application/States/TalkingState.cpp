8fc937a2149ea359d510f3e20374f67736540bf9
AMDevRadeon <100355451+AMDevRadeon@users.noreply.github.com>
2025-05-24 23:08:54 +0200

    feat: add sending messages ("calling") between two connected peers

--- src/UE/Application/States/TalkingState.cpp
+++ src/UE/Application/States/TalkingState.cpp
@@ -1,6 +1,7 @@
 #include "TalkingState.hpp"
 #include "ConnectedState.hpp"
 #include "Utils/todo.h"
+#include <format>
 
 namespace ue
 {
@@ -12,6 +13,46 @@ TalkingState::TalkingState(Context& context, common::PhoneNumber to) : BaseState
     logger.logInfo("Having call with: ", to);
     context.user.showCallTalkInterface();
     this->context.timer.startTimer(2min);
+}
+
+TalkingState::~TalkingState()
+{
+    /*
+     * Theoretically, there should be destructor defined at TimerPort
+     * that stops its timer, such that derived classes don't have to care about
+     * resetting it. BUT, because I don't know whether there is
+     * some miscellaneous interaction that needs to pass timer between
+     * states it will be done this way. 
+     */
+    this->context.timer.stopTimer();
+}
+
+void TalkingState::handleUiAction([[maybe_unused]] std::optional<std::size_t> selectedIndex)
+{
+    std::string const& user_mesg = this->context.user.getCallText();
+    logger.logInfo("User sent message: ", user_mesg);
+
+    this->resetTimer();
+
+    if (!user_mesg.empty())
+    {
+        this->context.user.appendCallText(std::format("[ME]: {}", user_mesg));
+        this->context.bts.sendCallTalk(this->to, user_mesg);
+        this->context.user.clearOutgoingCallText();
+        this->logger.logDebug("Message: \"", user_mesg, "\" succesfully sent to recipient at ", this->to.value);
+    }
+    else
+    {
+        this->logger.logInfo("Empty message: sending aborted");
+    }
+}
+
+void TalkingState::handleCallTalk(common::PhoneNumber to, const std::string& text)
+{
+    this->resetTimer();
+
+    this->context.user.appendCallText(std::format("[{}]: {}", this->to.value, text));
+    this->logger.logDebug("Message: \"", text, "\" succesfully received from caller at ", this->to.value);
 }
 
 void TalkingState::handleUiBack()
@@ -50,9 +91,20 @@ void TalkingState::handleTimeout()
 void TalkingState::endCall()
 {
     logger.logInfo("Ending call with: ", to);
+    
+    this->context.timer.stopTimer();
     context.bts.sendCallDropped(to);
     context.user.showEndedCall(to, "Call ended");
     context.setState<ConnectedState>();
+}
+
+void TalkingState::resetTimer(ue::ITimerPort::Duration inactivity_duration)
+{
+    logger.logDebug(std::format("Activity detected, resetting timer to {}", 
+                                std::chrono::duration_cast<std::chrono::seconds>(inactivity_duration)));
+
+    this->context.timer.stopTimer();
+    this->context.timer.startTimer(inactivity_duration);
 }
 
 }
--- src/UE/Application/States/TalkingState.hpp
+++ src/UE/Application/States/TalkingState.hpp
@@ -6,15 +6,27 @@ namespace ue {
 class TalkingState : public BaseState {
 public:
     TalkingState(Context& context, common::PhoneNumber to);
+    ~TalkingState() override;
+    /* Reenact rule of three
+     * I don't care that they don't keep inactivity time and instead
+     * reset timer to two minutes
+     */
+    TalkingState(TalkingState const& o) = default;
+    TalkingState& operator=(TalkingState const& o) = default;
+
     void handleUiBack() override;
-    void handleCallRequest(common::PhoneNumber from) override;
     void handleTimeout() override;
+    void handleCallRequest(common::PhoneNumber from) override;
     void handleCallDropped(common::PhoneNumber from) override;
     void handleUnknownRecipient(common::PhoneNumber from) override;
+    void handleCallTalk(common::PhoneNumber to, const std::string& text) override;
+    void handleUiAction(std::optional<std::size_t> selectedIndex) override;
 
 private:
     common::PhoneNumber to;
     void endCall();
+
+    void resetTimer(ue::ITimerPort::Duration inactivity_duration = std::chrono::minutes(2));
 };
 
 }
